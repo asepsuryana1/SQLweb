d2a2a4de33faad9c89451870f041a16b280953a1
asepsuryana1 <asepsuryana1@github.com>
2020-10-29 15:18:26 +0700

    fitur add with error parseint

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+var bodyParser = require('body-parser')
+const app = express()
 const port = 3000
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(path.join(__dirname, 'db/database12.db'));
  
 app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-  res.render('index', { data: [1,2,3] })
+  db.all('select * from siswa', (err, data) => {
+    if(err) return res.send (err)
+    console.log("hasilnya" ,data);
+    res.render('index', { data })
+  })
+ 
 })
 
 app.get('/add', function (req, res) {
-  res.render('form')
+  res.render('form', {
+    title:'form tambah'
+  })
 })
 
+app.post('/add', function (req, res) {
+  db.run('INSERT INTO siswa (nama, umur, tinggi,tanggallahir, ismenikah) values(?,?,?,?,?)', [req.body.nama, ParseInt(req.body.umur), ParseFloat(req.body.tinggi), req.body.tanggallahir, JSON.parse(req.body.ismenikah)], (err)=>{
+    if(err) return res.send(err)
+    res.redirect('/')
+  })
+  })
 app.listen(port, () => console.log(`example app listening on port ${port}`))
