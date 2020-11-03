6babfe40f0bdaf515704dde38f57b1ac0e1a5e46
AsepS <asepsuryana1@gmail.com>
2020-11-03 13:48:06 +0700

    almost done

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const moment = require('moment')
-
 const app = express()
 const port = 3000
 var sqlite3 = require('sqlite3').verbose();
@@ -20,44 +19,96 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
+  const {
+    cid,
+    id,
+    cnama,
+    nama,
+    cumur,
+    umur,
+    ctinggi,
+    tinggi,
+    ctanggallahir,
+    startdate,
+    enddate,
+    cismenikah,
+    ismenikah
+  } = req.query
+  let params = [];
+
+  if (cid && id) {
+    params.push(`id = ${id}`);
+  }
+  if (cnama && nama) {
+    params.push(`nama  like %${nama}%`);
+  }
+  if (cumur && umur) {
+    params.push(`umur  ${umur}`);
+  }
+  if (ctinggi && tinggi) {
+    params.push(`tinggi  ${tinggi}`);
+
+  }
+  if (ctanggallahir && startdate && enddate) {
+    params.push(`tanggallahir between '${startdate}' and '${enddate}'`);
+  }
+  if (cismenikah && ismenikah) {
+    params.push(`ismenikah  ${ismenikah}`);
+  }
+
   const page = req.query.page || 1;
   const limit = 3;
   const offset = (page - 1) * limit
 
-  db.all('SELECT COUNT(id) as total from siswa ', (err, data) => {
+  let sql = 'SELECT COUNT(id) as total from siswa';
+
+  if (params.length > 0) {
+    sql += ` where ${ params.join(' AND ')}`
+  }
+
+  db.all(sql, (err, data) => {
     if (err) return res.send(err)
     if (data.length == 0) res.send('data tidak ada')
     const total = data[0].total
     const pages = Math.ceil(total / limit)
 
-    db.all('select * from siswa limit ? offset ?',[limit,offset], (err, data) => {
+    sql = 'select * from siswa'
+    if (params.length > 0) {
+      sql += ` where ${ params.join(' AND ')}`
+    }
+    sql += ` limit ? offset ?`;
+
+
+    console.log(sql);
+    db.all(sql, [limit, offset], (err, data) => {
       if (err) return res.send(err)
       console.log("hasilnya", data);
       res.render('index', {
         data,
         moment,
         page,
-        pages
+        pages,
+        query: req.query
       })
     })
   })
 })
 
-app.get('/add', function (req, res) {
+app.get('/add', function(req, res) {
   res.render('form', {
     title: 'form tambah',
     item: {}
   })
 })
 
-app.post('/add', function (req, res) {
+app.post('/add', function(req, res) {
   db.run('INSERT INTO siswa (nama, umur, tinggi, tanggallahir, ismenikah) values(?,?,?,?,?)', [req.body.nama, parseInt(req.body.umur), parseFloat(req.body.tinggi), req.body.tanggallahir, JSON.parse(req.body.ismenikah)], (err) => {
     if (err) return res.send(err)
     res.redirect('/')
   })
 })
 
-app.get('/edit/:id', function (req, res) {
+app.get('/edit/:id', function(req, res) {
   db.all('SELECT * FROM siswa WHERE id=?', [parseInt(req.params.id)], (err, data) => {
     if (err) return res.send(err)
     if (data.length == 0) res.send('data tidak ada')
@@ -68,14 +119,14 @@ app.get('/edit/:id', function (req, res) {
   })
 })
 
-app.post('/edit/:id', function (req, res) {
+app.post('/edit/:id', function(req, res) {
   db.run('UPDATE siswa SET nama=?, umur=?, tinggi=?, tanggallahir=?, ismenikah=? WHERE id=?', [req.body.nama, parseInt(req.body.umur), parseFloat(req.body.tinggi), req.body.tanggallahir, JSON.parse(req.body.ismenikah), parseInt(req.params.id)], (err) => {
     if (err) return res.send(err)
     res.redirect('/')
   })
 })
 
-app.get('/delete/:id', function (req, res) {
+app.get('/delete/:id', function(req, res) {
   db.run('DELETE FROM siswa WHERE id=?', [parseInt(req.params.id)], (err, data) => {
     if (err) return res.send(err)
     res.redirect('/')
