b9be9d0439e0e11664569b457b5e3879a52a3a4a
docs <asepsuryana1@gmail.com>
2021-01-22 20:43:08 +0700

    add login

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.get('/',(req, res, next)=>{
+  res.render('login')
+})
+
 app.get('/', (req, res) => {
   const {
     cid,
